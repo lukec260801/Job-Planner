6f8e869d7ee82ae4c5463a1b778aa27585badcbd
lukec260801 <lukec260801@gmail.com>
2024-05-23 13:31:58 +0100

    changed list to table, got display working

--- src/main/java/com/luke/jobapp/JobAppController.java
+++ src/main/java/com/luke/jobapp/JobAppController.java
@@ -1,12 +1,15 @@
 package com.luke.jobapp;
 
-import javafx.beans.Observable;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.DatePicker;
-import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.util.Date;
 
 public class JobAppController {
     @FXML
@@ -19,18 +22,34 @@ public class JobAppController {
     private TextField local;
     @FXML
     private DatePicker date;
-    ObservableList<String> jobs = FXCollections.observableArrayList();
+    ObservableList<Job> jobs = FXCollections.observableArrayList();
     @FXML
-    private ListView<String> listView = new ListView<>(jobs);
+    private TableView<Job> table;
+    @FXML
+    private TableColumn<Job, String> colTitle;
+    @FXML
+    private TableColumn<Job, String> colCompany;
+    @FXML
+    private TableColumn<Job, Date> colDate;
+    @FXML
+    private TableColumn<Job, String> colStatus;
+    @FXML
+    private TableColumn<Job, String> colLocation;
 
     @FXML
     protected void submit() {
         Job newJob = new Job(title.getText(), company.getText(), date.getValue(), status.getText(), local.getText());
 
         System.out.println(newJob.getJobTitle());
-        jobs.add(0, newJob.getJobTitle());
+        jobs.add(newJob);
 
-        listView.refresh();
+        colTitle.setCellValueFactory(new PropertyValueFactory<>("jobTitle"));
+        colCompany.setCellValueFactory(new PropertyValueFactory<>("company"));
+        colDate.setCellValueFactory(new PropertyValueFactory<>("dateApplied"));
+        colStatus.setCellValueFactory(new PropertyValueFactory<>("status"));
+        colLocation.setCellValueFactory(new PropertyValueFactory<>("location"));
+
+        table.setItems(jobs);
 
         title.clear();
         company.clear();
